1b79bf70be328633190f8adea3401bb8f4f19bdb
javiipzo <javipozo999@gmail.com>
2022-10-14 17:48:17 +0200

    subirtodo

--- application/controllers/controller_contactos.py
+++ application/controllers/controller_contactos.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import Flask, jsonify#importamos los modulos necesarios
+from flask import Flask, jsonify #importamos los modulos necesarios
 from flask import render_template, request, redirect, url_for, flash, session
 from application.models.model_contactos import Contactos
 from application.config.mongodb import ConexionMongo
